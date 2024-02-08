704bf8c35f9e0230937fa0b6dcd8bb577c36e397
sebtech974 <sebtech974@gmail.com>
2024-02-08 08:24:03 +0400

    résolution probleme formulaire + derniere image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,11 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data?.events.sort((evtA, evtB) =>
+  new Date(evtB.Date) - new Date(evtA.date)
+  )[0];
+
   return <>
     <header>
       <Menu />
@@ -116,13 +120,17 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
+
+        { last &&(
+          <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="boom"
+          label="Dernier évènement"
         />
+        )}
+       
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
